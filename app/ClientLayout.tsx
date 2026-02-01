5b4146b3b8be70238cace672f5183b433273f144
Alanood Alsanea <alsanea.alanoodgmail.com@Alanoods-MacBook-Pro.local>
2026-02-01 14:04:55 +0300

    Split pages into /brands and /customer, remove nav

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,18 +1,11 @@
 'use client'
 
-import { useState } from 'react'
-import Header from '@/components/layout/Header'
-import SideMenu from '@/components/layout/SideMenu'
 import BasketDrawer from '@/components/ui/BasketDrawer'
 import { BasketProvider } from '@/contexts/BasketContext'
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const [drawerOpen, setDrawerOpen] = useState(false)
-
   return (
     <BasketProvider>
-      <Header onMenuToggle={() => setDrawerOpen(true)} />
-      <SideMenu isOpen={drawerOpen} onClose={() => setDrawerOpen(false)} />
       <BasketDrawer />
       {children}
     </BasketProvider>
